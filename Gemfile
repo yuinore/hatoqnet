012c42f6e8609228213248d887cf01d7d83991d5
yuinore <yuinore@hotmail.co.jp>
2021-02-24 11:25:45 -0500

    add gem annotate

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'slim-rails'
 gem 'html2slim'
+
+gem 'annotate'
--- app/models/top_link.rb
+++ app/models/top_link.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: top_links
+#
+#  id           :bigint           not null, primary key
+#  color        :string(255)
+#  name         :string(255)
+#  shadow_color :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class TopLink < ApplicationRecord
 end
