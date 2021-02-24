eb5723be91f985d4b076e217d2bb65c895b99c85
yuinore <yuinore@hotmail.co.jp>
2021-02-24 10:30:38 -0500

    add RootController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :root, only: [:index]
+
+  root to: 'root#index'
 end
