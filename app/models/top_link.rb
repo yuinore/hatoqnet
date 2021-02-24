539ea7ee20aed1d46460f32dfcb5bd9b62ffd721
yuinore <yuinore@hotmail.co.jp>
2021-02-24 12:08:16 -0500

    fix column order

--- app/models/top_link.rb
+++ app/models/top_link.rb
@@ -3,9 +3,9 @@
 # Table name: top_links
 #
 #  id           :bigint           not null, primary key
-#  color        :string(255)
-#  link         :string(255)
 #  name         :string(255)
+#  link         :string(255)
+#  color        :string(255)
 #  shadow_color :string(255)
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
