2676457b792d66b288b222a897764921044038df
yuinore <yuinore@hotmail.co.jp>
2021-03-09 21:50:29 +0900

    add fc2 HP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@ TopLink.create!(name: "ニジエ",      link: "https://nijie.info/members.php?id
 TopLink.create!(name: "Melonbooks",  link: "https://www.melonbooks.co.jp/circle/index.php?circle_id=41547", color: "#ADEFAB", shadow_color: "#99D397")
 TopLink.create!(name: "BOOTH",       link: "https://hatoqne.booth.pm",                                      color: "#FFB5AA", shadow_color: "#E1A096")
 TopLink.create!(name: "pixivFANBOX", link: "https://hatoqne-dm.fanbox.cc",                                  color: "#FFE17E", shadow_color: "#E1C76F")
+TopLink.create!(name: "脱衣ブロック崩し", link: "https://hatoqne.x.fc2.com",                                color: "#C7EC91", shadow_color: "#B0D080")
 TopLink.create!(name: "Pawoo",       link: "https://pawoo.net/@hatoqne_dm",                                 color: "#AFACC8", shadow_color: "#9A98B0")
 TopLink.create!(name: "plurk",       link: "https://www.plurk.com/m/hatoqne_dm",                            color: "#F1A591", shadow_color: "#D59280")
