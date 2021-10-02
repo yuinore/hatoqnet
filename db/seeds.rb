6e51d892af59bda180e7a8103547ef0ec58446c2
yuinore <yuinore@hotmail.co.jp>
2021-10-03 07:06:34 +0900

    Twitterリンクを修正・リンクにとらのあなを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 TopLink.destroy_all
-TopLink.create!(name: "Twitter",     link: "https://twitter.com/hatoqne_dm",                                color: "#ADE7FF", shadow_color: "#99CCE1")
+TopLink.create!(name: "Twitter",     link: "https://twitter.com/hatoqne",                                color: "#ADE7FF", shadow_color: "#99CCE1")
 TopLink.create!(name: "pixiv",       link: "https://pixiv.me/hatoqne",                                      color: "#B1C7FF", shadow_color: "#9CB0E1")
 TopLink.create!(name: "ニジエ",      link: "https://nijie.info/members.php?id=71502",                       color: "#FFC0D0", shadow_color: "#E1A9B8")
 TopLink.create!(name: "Melonbooks",  link: "https://www.melonbooks.co.jp/circle/index.php?circle_id=41547", color: "#ADEFAB", shadow_color: "#99D397")
+TopLink.create!(name: "とらのあな", link: "https://ec.toranoana.jp/tora_r/ec/cot/circle/2UPA216Q8X74dE60d687/all/", color: "#FDCC76", shadow_color: "#DFB468")
 TopLink.create!(name: "BOOTH",       link: "https://hatoqne.booth.pm",                                      color: "#FFB5AA", shadow_color: "#E1A096")
 TopLink.create!(name: "pixivFANBOX", link: "https://hatoqne-dm.fanbox.cc",                                  color: "#FFE17E", shadow_color: "#E1C76F")
 TopLink.create!(name: "脱衣ブロック崩し", link: "https://hatoqne.x.fc2.com",                                color: "#C7EC91", shadow_color: "#B0D080")
