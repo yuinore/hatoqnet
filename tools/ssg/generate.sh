5464c95091c51cfa1e369d3ff17434a87cbe5e85
yuinore <yuinore@hotmail.co.jp>
2022-03-24 01:48:47 +0900

    fix remote

--- tools/ssg/generate.sh
+++ tools/ssg/generate.sh
@@ -5,4 +5,5 @@ set -eux
 
 rm -rf dist
 # wget -r -l inf -k -nc -nH -p -E -e robots=off -P ./dist http://localhost:3000
-wget -r -l inf -k -nc -nH -p -E -e robots=off -P ./dist https://hatoq.net
+HEROKU_REPO=heroku-chino
+wget -r -l inf -k -nc -nH -p -E -e robots=off -P ./dist "https://${HEROKU_REPO}.herokuapp.com/"
