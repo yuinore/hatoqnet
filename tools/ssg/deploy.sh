46ed1cbeba043815d27f47a9463afb7ce957f5c3
yuinore <yuinore@hotmail.co.jp>
2022-03-24 03:30:52 +0900

    fix

--- tools/ssg/deploy.sh
+++ tools/ssg/deploy.sh
@@ -6,17 +6,28 @@ git stash --include-untracked
 git checkout gh-pages
 
 if [ -e CNAME ]; then
+  git rm -rf .
+  git stash --include-untracked
   rm -rf *
+
   git checkout HEAD -- CNAME
 else
+  git rm -rf .
+  git stash --include-untracked
   rm -rf *
 fi
 
-git stash pop
+git stash pop stash@{1}
+
 mv dist/* .
 rmdir dist
 git add .
 git commit -m 'Generate Static Site'
 git pull
 git push
+
+git checkout HEAD^
+git stash pop stash@{0}
+git reset --hard HEAD
+
 git checkout master
