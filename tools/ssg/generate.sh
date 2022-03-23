b62d15e521b3c54c3d5bbf6901061f39fa0dc1bc
yuinore <yuinore@hotmail.co.jp>
2022-03-24 02:13:56 +0900

    ページ上部へのリンクが index.html へのリンクになってしまうので対症療法

--- tools/ssg/generate.sh
+++ tools/ssg/generate.sh
@@ -7,3 +7,6 @@ rm -rf dist
 # wget -r -l inf -k -nc -nH -p -E -e robots=off -P ./dist http://localhost:3000
 HEROKU_REPO=heroku-chino
 wget -r -l inf -k -nc -nH -p -E -e robots=off -P ./dist "https://${HEROKU_REPO}.herokuapp.com/"
+
+# ページ上部へのリンクが index.html へのリンクになってしまうので対症療法
+sed -i 's/index.html#page-top/#page-top/g' dist/index.html
